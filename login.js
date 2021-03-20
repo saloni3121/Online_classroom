57c18988eeb61ac8dd679982def83779b76cc4ac
SaloniPatel <saloni.patel01@gmail.com>
2021-03-20 14:17:15 +0530

    first

---
+++ login.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+    });
+    $(".toggle-password1").click(function() {
+
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+});
